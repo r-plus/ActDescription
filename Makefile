4965d90b63c7f2dbff51cf69a36b31e7e5ac9498
r-plus <anasthasia.r@gmail.com>
2012-12-05 21:55:40 +0900

    initial commit

---
+++ Makefile
@@ -0,0 +1,11 @@
+ARCHS = armv7
+TARGET = iphone:clang::5.0
+include theos/makefiles/common.mk
+
+TWEAK_NAME = ActDescription
+ActDescription_FILES = Tweak.mm
+ActDescription_FRAMEWORKS = UIKit
+ActDescription_LIBRARIES = activator
+ActDescription_CFLAGS = -fobjc-arc
+
+include $(THEOS_MAKE_PATH)/tweak.mk
---
+++ Tweak.mm
@@ -0,0 +1,62 @@
+#import <notify.h>
+#import <UIKit/UIKit.h>
+#import <libactivator/libactivator.h>
+
+#define kActivatorAction "jp.r-plus.actdescription"
+
+static BOOL isActive = NO;
+
+@interface UIView(Private)
+- (id)recursiveDescription;
+@end
+
+@interface ActDescription : NSObject <LAListener>
+@end
+
+static void PrintDescription(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo)
+{
+  NSLog(@"%@", [[UIApplication sharedApplication].keyWindow recursiveDescription]);
+}
+
+static void WillEnterForegroundNotificationReceived(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo)
+{
+  if (!isActive) {
+    isActive = YES;
+    CFNotificationCenterRef darwin = CFNotificationCenterGetDarwinNotifyCenter();
+    CFNotificationCenterAddObserver(darwin, NULL, PrintDescription, CFSTR(kActivatorAction), NULL, CFNotificationSuspensionBehaviorCoalesce);
+  }
+}
+
+static void DidEnterBackgroundNotificationReceived(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo)
+{
+  if (isActive) {
+    isActive = NO;
+    CFNotificationCenterRef darwin = CFNotificationCenterGetDarwinNotifyCenter();
+    CFNotificationCenterRemoveObserver(darwin, (const void *)PrintDescription, CFSTR(kActivatorAction), NULL);
+  }
+}
+
+@implementation ActDescription
++ (void)load
+{
+  @autoreleasepool {
+    if (LASharedActivator.runningInsideSpringBoard) {
+      ActDescription *listener = [[self alloc] init];
+      [LASharedActivator registerListener:listener forName:@kActivatorAction];
+    } else {
+      CFNotificationCenterRef local = CFNotificationCenterGetLocalCenter();
+      CFNotificationCenterAddObserver(local, NULL, WillEnterForegroundNotificationReceived, (CFStringRef)UIApplicationDidFinishLaunchingNotification, NULL, CFNotificationSuspensionBehaviorCoalesce);
+      CFNotificationCenterAddObserver(local, NULL, WillEnterForegroundNotificationReceived, (CFStringRef)UIApplicationWillEnterForegroundNotification, NULL, CFNotificationSuspensionBehaviorCoalesce);
+      CFNotificationCenterAddObserver(local, NULL, DidEnterBackgroundNotificationReceived, (CFStringRef)UIApplicationDidEnterBackgroundNotification, NULL, CFNotificationSuspensionBehaviorCoalesce);
+    }
+  }
+}
+
+- (void)activator:(LAActivator *)activator receiveEvent:(LAEvent *)event forListenerName:(NSString *)listenerName
+{
+  if ([listenerName isEqualToString:@kActivatorAction]) {
+    notify_post(kActivatorAction);
+    event.handled = YES;
+  }
+}
+@end
